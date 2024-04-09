2d43a1efc436f3d9202b4651ee4a9e1cdd733229
DIBAKAR DAS <careofdibakar@gmail.com>
2024-04-09 19:38:55 +0530

    view file rendered in node js, ejs used

---
+++ check.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+console.log("cv_summery")
+
+// app.post('/check', (req, res) => {
+//     // const cv_summery = req.body.cv_summery
+//     // console.log(cv_summery)
+//     console.log("cv_summery")
+// });
+
---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 8000; // Or any port you prefer
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+
+// Define routes
+app.get('/', (req, res) => {
+    // Render the index.ejs file
+    res.render('index', { title: 'ATS' });
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
